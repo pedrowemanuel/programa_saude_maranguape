d9bd2a20a9bb2a44781d918c86d021ae0798be68
pedrowemanuel <pedronuel36@gmail.com>
2022-02-12 10:46:14 -0300

    Iniciado implementação Controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,9 +21,41 @@ import java.text.SimpleDateFormat;
 /**
  * Servlet implementation class Controller
  */
-@WebServlet(urlPatterns = { "/Controller", "/index" })
+@WebServlet(urlPatterns = {
+		"/acessarLoginAdmin",
+		"/acessarLoginProfissionalSaude",
+		"/cidadao/selecionarUnidade",
+		"/admin/fazerLogin",
+		"/funcionario/fazerLogin",
+		"/admin/acessarCadastroUnidade",
+		"/admin/cadastrarUnidade",
+		"/admin/acessarListarUnidades",
+		"/admin/excluirUnidade",
+		"/admin/acessarCadastroFuncionarioAdmin",
+		"/admin/cadastrarFuncionarioAdmin",
+		"/admin/acessarListarFuncionariosAdmin",
+		"/admin/excluirFuncionario",
+		"/funcionarioAdmin/acessarCadastroFuncionario",
+		"/funcionarioAdmin/cadastrarFuncionario",
+		"/funcionarioAdmin/acessarListarFuncionarios",
+		"/funcionarioAdmin/acessarCadastroPostagem",
+		"/funcionarioAdmin/cadastrarPostagem",
+		"/funcionarioAdmin/acessarListarPostagens",
+		"/funcionarioAdmin/excluirPostagem",
+		"/funcionario/acessarCadastroPostagem",
+		"/funcionario/cadastrarPostagem",
+		"/funcionario/acessarListarPostagens",
+		"/funcionario/excluirPostagem",
+		"/cidadao/acessarPostagens",
+		"/cidadao/acessarComentarios",
+		"/cidadao/acessarCadastroComentario",
+		"/cidadao/cadastrarComentario",
+		"/cidadao/cadastrarRespostaComentario",
+		"/deslogar"
+	})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	protected static boolean logado = false;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -38,6 +71,13 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String action = request.getServletPath();
+		
+		if (action.equals("/acessarLoginAdmin")) {
+			request.setAttribute("tipoLogin", "admin");
+
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("login.jsp");
+			requestDispatcher.forward(request, response);
+		}
 	
 	}
 
